b18f3837ce9b678972e5c6b9a1289e4583ed1fe8
Leandro Lourenci <leandro@lourenci.com>
2018-06-23 09:35:24 -0300

    Configure dependency in install.sh file

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+brew install the_silver_searcher
+
 ln -s ~/Developer/dotfiles/vim/vim.symlink ~/.vim
 ln -s ~/Developer/dotfiles/vim/vimrc.symlink ~/.vimrc
 ln -s ~/Developer/dotfiles/vim/gvimrc.symlink ~/.gvimrc
