312540213cb72a9b973d2508871fb5e59c9187c2
Leandro Lourenci <leandro@lourenci.com>
2019-03-12 12:02:14 -0300

    Better install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-brew install cmake # Necessary to install YCM
 brew install the_silver_searcher # To use ag inside fzf
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags # To use :BTags inside fzf
 
 ln -s ~/Developer/dotfiles/vim/vim.symlink ~/.vim
 ln -s ~/Developer/dotfiles/vim/vimrc.symlink ~/.vimrc
