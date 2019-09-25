fc14f2c1eaa720ed3997124d491de6aefa4fa65a
Leandro Lourenci <leandro@lourenci.com>
2019-09-25 09:32:54 -0300

    Remove universal-ctags

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 brew install the_silver_searcher # To use ag inside fzf
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags # To use :BTags inside fzf
 
 ln -s ~/Developer/dotfiles/vim/vim.symlink ~/.vim
 ln -s ~/Developer/dotfiles/vim/vimrc.symlink ~/.vimrc
