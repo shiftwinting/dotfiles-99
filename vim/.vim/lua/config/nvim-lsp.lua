ca94797d2f058f64a62e6b11030739aee4f46adc
Leandro Lourenci <leandro@lourenci.com>
2021-07-27 17:36:01 -0300

    remove unecessary lsp config

--- vim/.vim/lua/config/nvim-lsp.lua
+++ vim/.vim/lua/config/nvim-lsp.lua
@@ -3,13 +3,6 @@ local lspconfig = require'lspconfig'
 local flags = { debounce_text_changes = 150 }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  }
-}
 
 lspconfig.tsserver.setup{
   on_attach = function(client)
