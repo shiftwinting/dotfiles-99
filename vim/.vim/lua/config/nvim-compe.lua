ce18776e3b44450ce8e6f066c2f12882090ec39c
Leandro Lourenci <2339362+lourenci@users.noreply.github.com>
2021-08-18 18:16:50 -0300

    fix mappings

--- vim/.vim/lua/config/nvim-compe.lua
+++ vim/.vim/lua/config/nvim-compe.lua
@@ -10,8 +10,6 @@ cmp.setup {
   mapping = {
     ['<C-p>'] = cmp.mapping.prev_item(),
     ['<C-n>'] = cmp.mapping.next_item(),
-    ['<C-d>'] = cmp.mapping.scroll(-4),
-    ['<C-f>'] = cmp.mapping.scroll(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<C-y>'] = cmp.mapping.confirm({
